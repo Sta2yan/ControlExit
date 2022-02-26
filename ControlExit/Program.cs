4e9bbd576a12456e5710bfa386d06b6587583f27
Sta2yan <s2_stas@mail.ru>
2022-02-26 23:01:46 +0300

    Добавьте файлы проекта.

---
+++ ControlExit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlExit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userText;
+            int countTextOutput;
+            string exitCommand = "exit";
+
+            while (true)
+            {
+                Console.Write("Введите текст для вывода: ");
+                userText = Console.ReadLine();
+
+                if(userText.Equals(exitCommand))
+                    break;
+
+                Console.Write("Введите сколько раз нужно вывести текст: ");
+                countTextOutput = Convert.ToInt32(Console.ReadLine());
+
+                if (countTextOutput.Equals(exitCommand))
+                    break;
+
+                for (int i = 0; i < countTextOutput; i++) Console.WriteLine((i + 1) + ". " + userText);
+            }
+
+            Console.WriteLine("Выход...");
+        }
+    }
+}
